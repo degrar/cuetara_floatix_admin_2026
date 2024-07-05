d293d183845b841be2547ee53937a8d8289d5890
Gerard A <gerard.a.c@gmail.com>
2024-07-05 11:48:49 +0200

    legal

--- resources/js/Pages/Public/Legal.vue
+++ resources/js/Pages/Public/Legal.vue
@@ -1,97 +1,10 @@
 <template>
     <GuestLayout title="Bases legales de la promoción" menu>
-        <div class="bg-white" :style="{ marginTop: headerHeight + 'px' }">
-            <div class="container mx-auto px-[15px]">
-                <div class="flex justify-center flex-wrap items-stretch w-full">
-                    <img :src="getImagePath('logos')" alt="50%" class="w-[620px] mx-auto pb-[15px]"/>
-                </div>
-            </div>
-        </div>
-        <div class="backgroundpattern bg-center bg-no-repeat"></div>
+        <InsideHeaderPage />
         <div class="bg-yellow">
             <div class="container mx-auto px-[15px] flex justify-center items-center flex-col py-[40px] lg:flex-row lg:py-[80px]">
                 <div class="w-12/12  pt-[15px]">
-                    <h1 class="text-brown uppercase font-bemio rem:text-[26px] rem:leading-[30px] lg:rem:text-[50px] lg:rem:leading-[54px] tracking-[1px] text-center p-0 m-0 mb-5">BASES LEGALES DE LA PROMOCIÓN</h1>
-                    <h2 class="text-brown uppercase font-bemio rem:text-[22px] rem:leading-[30px] lg:text-[28px] lg:rem:leading-[36px] tracking-[1px] text-center p-0 m-0">SORPRESARIO KINDER JOY HARRY POTTER QUIDDITCH</h2>
-                    <h2 class="text-brown font-bemio rem:text-[22px] rem:leading-[30px] lg:text-[28px] md:rem:leading-[36px] tracking-[1px] text-center p-0 mb-10">Del 20 de mayo de 2024 a 7 de julio de 2024</h2>
-
-                    <h4>I. OBJETO DE LA PROMOCIÓN</h4>
-                    <p>FERRERO IBÉRICA, S.A, con domicilio fiscal y administrativo en WTC Almeda Park, Plaça de la Pau s/n Edificio 2, Planta 3ª, 08940 Cornellà de Llobregat (Barcelona) España, con NIF nº A-58536111, comercializador en España de productos alimenticios, relacionados con la chocolatería, confitería y pastelería (en adelante, el “Organizador o “FERRERO”), ha puesto en marcha la promoción SORPRESARIO KINDER JOY HARRY POTTER QUIDDITCH (en adelante la “Promoción”) para promocionar la marca Kinder® y las sorpresas de la colección de Kinder® Joy con la licencia Harry Potter Quidditch en España.</p>
-
-                    <h4>II. ÁMBITO TERRITORIAL Y TEMPORAL DE LA PROMOCIÓN</h4>
-                    <p>2.1. Ámbito Territorial: España (todo el territorio nacional).</p>
-                    <p>2.2. Ámbito de Aplicación: La Promoción está dirigida a aquellas personas físicas mayores de dieciocho (18) años residentes en España y que sean seguidores de la cuenta oficial de Kinder® España en Instagram®.</p>
-                    <p>2.3. Ámbito temporal: Esta promoción tiene una duración de 7 semanas, del 20 de mayo al 7 de julio.</p>
-                    <p>La promoción empezará el 20 de mayo a las 9:00h de la mañana, finalizando el 7 de julio de 2024 a las 23:59 horas, ambos inclusive (en adelante, el “Periodo Promocional 1”).</p>
-
-                    <h4>III. REQUISITOS PARA PARTICIPAR EN LA PROMOCIÓN</h4>
-                    <p>3.1. La participación en la presente Promoción es de carácter gratuito y está dirigida a aquellas personas físicas de dieciocho (18) años o más que se encuentren registrados en la plataforma Instagram® y residan en España.</p>
-                    <p>3.2. En particular, FERRERO se reserva el derecho a descalificar a cualquier participante que manipule los  procedimientos de registro y/o que viole cualquiera de las bases contenidas en el presente documento. Asimismo, FERRERO también se reserva el derecho a verificar por cualquier procedimiento que estime apropiado que el ganador cumple con todos los requisitos de este documento y que los datos que proporcione a FERRERO son exactos y verídicos. Entre otras cuestiones, FERRERO podrá pedir documentación a los participantes para que acrediten el cumplimiento de los requisitos para participar. La no aportación de esta documentación podrá dar lugar a la descalificación del participante requerido.</p>
-                    <p>3.3. Quedan excluidos de esta Promoción los propietarios, empleados, y otros colaboradores de FERRERO que hayan participado en el desarrollo y gestión de la presente Promoción, así como sus respectivos familiares ascendientes, descendientes, cónyuges o parejas de hecho. Asimismo, el organizador se reserva el derecho a evitar que sobre esta Promoción se establezcan sistemas de especulación o de negocios, en cuyo caso, podrá ejercer aquellas acciones legales que estime oportunas.</p>
-                    <p>3.4. FERRERO descartará aquellas participaciones que sean abusivas o fraudulentas. Se entenderá, a título enunciativo, pero no limitativo, que se produce fraude cuando se detecta el supuesto uso de aplicaciones independientes al sitio de la Promoción; el abuso de consultas al servidor y todos aquellos comportamientos que puedan resultar aparentemente abusivos y/o malintencionados. La constatación de cualquiera de estas circunstancias durante la Promoción supondrá la descalificación automática, así como la pérdida de los Premios si se le hubiere otorgado.</p>
-                    <p>3.5. Si se detecta que cualquiera de las publicaciones propuestas, a juicio de FERRERO, atenta contra el derecho al honor, a la intimidad personal y familiar, a la propia imagen, al respeto a la dignidad de la persona, al principio de no discriminación por motivos de raza, sexo, religión, opinión, nacionalidad, discapacidad o cualquier otra circunstancia personal o social o a la buena fe y/o vulneren cualquier elemento u objeto tutelado por derechos de exclusividad, propiedad industrial o intelectual o cualquier otro derecho de terceros, se procederá a la inmediata descalificación del usuario sin que le asista derecho de reclamación alguno. También será causa de descalificación la publicación o el intento de publicación por parte del usuario participante de cualquier contenido que a juicio de FERRERO dañe o pudiera menoscabar la reputación y buena imagen de FERRERO y/o de cualquiera de sus marcas.</p>
-                    <p>3.6. FERRERO se reserva el derecho de emprender acciones judiciales contra aquellas personas que realicen cualquier tipo de acto susceptible de ser considerado manipulación o falsificación de la Promoción.</p>
-
-                    <h4>IV. MECÁNICA DE LA PROMOCIÓN</h4>
-                    <p>4.1. Todas las personas que cumplan con los requisitos anteriormente mencionados, y que deseen participar en la Promoción, deberán estar atentos a las publicaciones del perfil oficial de Kinder® en Instagram® (@kinder.spain) y proceder según se indique. Se recomienda a los participantes, si no lo hacen ya, seguir a la cuenta de @kinder.spain en Instagram®.</p>
-                    <p>4.2. Durante toda la Promoción, el participante deberá:</p>
-                    <ul>
-                        <li>Encontrar una figura dorada de la colección de Kinder Joy con Harry Potter.</li>
-                        <li>Fotografiar la figura</li>
-                        <li>Escanear el código QR que aparece en los materiales de punto de venta de Kinder Joy, que le conducirá a una landing page promocional. También será posible acceder a través de la web www.kinder.es;</li>
-                        <li>Subir la foto e introducir los datos requeridos para gestionar la participación.</li>
-                    </ul>
-                    <p>4.3. Se contabilizará una única participación por nombre de usuario en todo el Periodo Promocional. Cada usuario podrá resultar ganador de un único premio en toda la Promoción.</p>
-                    <p>4.4. Todas aquellas publicaciones que no contemplen ninguno o alguno de los puntos del apartado 4, a excepción del apartado anterior que no es obligatorio, no se tendrán en consideración a efectos de participación en la presente Promoción.</p>
-
-                    <h4>V. PREMIO</h4>
-                    <p>5.1. Entre todos los usuarios que hayan participado en la promoción SORPRESARIO KINDER JOY HARRY POTTER QUIDDITCH, de acuerdo con lo especificado en las presentes Bases Legales, se premiará con:</p>
-                    <ul>
-                        <li>Un “Sorpresario” con toda la colección de sorpresas de Kinder Joy con la licencia HARRY POTTER QUIDDITCH valorado en 50 euros (sin considerar IVA y coste de transporte) para cada ganador.</li>
-                    </ul>
-
-                    <p>5.2. Se entregarán por sistema de momento ganador un total de catorce (14) Sorpresarios, a razón de 2 por semana, durante las 7 semanas de la promoción. Incluye la colección de sorpresas “Harry Potter Quiddich” completa (en adelante, el “Premio”). Por tanto 2 Sorpresarios con la colección completa por semana.</p>
-                    <p>5.3. En ningún caso, se aceptarán ni se gestionarán cesiones del mismo a terceros ni tendrán los participantes ganadores el derecho a recibir su valor en metálico.</p>
-
-                    <h4>VI. SELECCIÓN DE LOS GANADORES Y ENTREGA DE LOS PREMIOS</h4>
-
-                    <p>6.1. Para la selección de los ganadores, se utilizará el sistema de Momento Ganador.</p>
-                    <p>6.2. En el caso de que, por cualquier motivo (principalmente, por no registrarse participaciones entre un momento ganador y otro) un premio quedase desierto, Ferrero se reserva la posibilidad de reintroducir Momentos Ganadores en la siguiente semana de la promoción. Asimismo, en caso de dar 1 o varios premios desiertos al final de la promoción, Ferrero se reserva el derecho de realizar un sorteo final.</p>
-                    <p>6.3. Una vez seleccionados los ganadores, FERRERO se pondrá en contacto con ellos (en adelante, los “Ganadores”), a efectos de comunicarles que han resultado premiados y gestionar la     correcta entrega del Premio. Estos deberán comunicar a FERRERO en el plazo máximo de siete (7) días, la aceptación del Premio; una dirección de envío y una fotocopia de su DNI, pasaporte o cualquier otro documento identificativo equivalente u otra documentación necesaria para participar.</p>
-                    <p>6.4. Si transcurrido el plazo de siete días arriba señalado no se recibiera respuesta por parte del Ganador, se entenderá que renuncia al Premio, perdiendo todo derecho al mismo. En tal caso, el Premio quedará desierto.</p>
-                    <p>6.5. Asimismo, se comunicará a todos los participantes el nombre de los Ganadores en el perfil oficial de Kinder® España en Instagram®, por lo que los Ganadores aceptan que sus nombres /usuarios se publiquen en los mismos medios por los que se ha dado a conocer la Promoción con fines de transparencia.</p>
-                    <p>6.6. Sólo se realizará el envío del Premio dentro del territorio de ESPAÑA. El envío será totalmente gratuito y no comportará ningún coste para el participante Ganador. El Premio se enviará al domicilio que los Ganadores indiquen a FERRERO en el momento de aceptar el Premio. Dicho domicilio deberá ser dentro del territorio de España.</p>
-                    <p>6.7. La entrega del Premio a los Ganadores se realizará en el plazo máximo de un (1) mes desde la fecha en que el participante haya proporcionado los datos de contacto y FERRERO haya validado que son correctos, FERRERO se reserva el derecho a prolongar dicho plazo debido a complicaciones de entrega. No obstante, si efectuados dos (2) intentos de entrega del Premio al ganador en la dirección indicada, dicha entrega no se llega a realizar por causas imputables al ganador, éste perderá todo derecho de recibir el Premio, así ́ como de ejercitar cualquier reclamación relacionada. Sin menoscabo de otras posibles causas de falta de entrega que sean consideradas imputables al ganador, se considerarán causas imputables al participante el haber proporcionado a FERRERO unos datos de localización erróneos.</p>
-                    <p>6.8. FERRERO no se responsabilizará de las posibles incidencias en la entrega o cualquier otra circunstancia imputable a terceros que afecte a los premios de esta Promoción.</p>
-
-                    <h4>VII. DISPOSICIONES GENERALES Y LIMITACIÓN DE RESPONSABILIDAD</h4>
-                    <p>7.1. FERRERO se reserva el derecho de variar los Premios y/o características del mismo, si fuera necesario, por cualquier causa.</p>
-                    <p>7.2. FERRERO hará todo lo posible por evitar el suministro de los datos o documentos incorrectos o falsos por parte de los Ganadores. Asimismo, FERRERO no se hará responsable de la veracidad de los datos o documentos que los participantes faciliten. Por consiguiente, si los datos o documentos facilitados no fueran correctos o tuvieran errores, FERRERO no se hará responsable de no poder contactar con los Ganadores para gestionar con ellos la entrega de los Premios y de todas las incidencias que pudieran derivarse de la incorrección o falsedad de los datos o documentos suministrados.</p>
-                    <p>7.3. El plazo máximo establecido para cualquier tipo de reclamación referida y/o relacionada con la Promoción se extingue a los dos (2) meses desde la finalización de la Promoción, una vez finalizado dicho plazo, se perderá todo derecho a reclamación.</p>
-
-                    <h4>VIII. ACEPTACIÓN DE LAS BASES LEGALES</h4>
-                    <p>8.1. La participación en la presente Promoción del usuario implica obligatoriamente la aceptación íntegra de las presentes bases y de las correspondientes condiciones particulares que se detallarán en el story en Instagram®. FERRERO se reserva el derecho a variar estas bases si así lo exigieran las circunstancias y dirimir cuantas dudas pudieran surgir en lo no previsto en las mismas.</p>
-                    <p>8.2. FERRERO se reserva también el derecho de anular, prorrogar, ampliar, recortar o modificar la Promoción si las circunstancias lo obligan, sin tener que justificar la decisión y sin que se le pueda reclamar ningún tipo de responsabilidad como consecuencia de ello, siempre que no perjudiquen los derechos adquiridos por los participantes.</p>
-                    <p>8.3. Las Bases Legales permanecerán publicadas durante el Período Promocional en el link de la “bio”del perfil oficial de Kinder® España en Instagram®, así como en la landing promocional.</p>
-
-                    <h4>IX. ELEMENTOS DE PUBLICIDAD</h4>
-                    <p>9.1. La Promoción “SORPRESARIO KINDER JOY Harry Potter Quidditch” así como la acción en su conjunto será comunicado en publicaciones del perfil oficial de Kinder España a través de Instagram® y en el sitio web www.kinder.es .</p>
-
-                    <h4>X. PROTECCIÓN DE DATOS</h4>
-                    <p>10.1. FERRERO tratará los datos personales que los participantes nos proporcionen con el fin de gestionar correctamente la presente Promoción, identificación de los Ganadores, adjudicación y gestión de los Premios. FERRERO se encuentra legitimado para tratar los datos personales de los participantes con el fin de gestionar correctamente la participación de la presente Promoción.</p>
-                    <p>10.2. En cualquier momento, los participantes tienen derecho a ejercer sus derechos de acceso, rectificación y supresión, limitación del tratamiento, portabilidad, oposición, mediante petición escrita a la siguiente dirección de correo electrónico; privacy.es@ferrero.com.</p>
-                    <p>10.3. En todo caso, para más información, todos los participantes podrán consultar la información adicional y detallada sobre la política de protección de datos en el footer de la página web www.kinder.es.</p>
-
-                    <h4>XI. RÉGIMEN FISCAL</h4>
-                    <p>11.1. Los Premios objeto de la presente Promoción no se encuentran sujetos a retención ni a ingreso a cuenta del Impuesto sobre la Renta de las Personas Físicas de acuerdo con el artículo 75.3.f) del Real Decreto 439/2007, por el que se aprueba el Reglamento del IRPF, por ser su base de retención inferior a trescientos (300) euros.</p>
-
-                    <h4>XII. EXONERACIÓN DE RESPONSABILIDAD</h4>
-                    <p>12.1. FERRERO exonera expresa y completamente a Instagram® de toda responsabilidad por parte de la actuación y contenidos que cada concursante o participante pueda subir a la red social.</p>
-                    <p>12.2.   La presente Promoción no está patrocinada, avalada, administrada ni asociada en modo alguno a Instagram®. En este sentido, los participantes exoneran a Instagram® de toda responsabilidad derivada de su participación en la Promoción.</p>
-
-                    <h4>XIII. LEY APLICABLE Y JURISDICCIÓN</h4>
-                    <p>13.1. Las presentes Bases Legales se regirán e interpretarán de acuerdo con su propio contenido y por las leyes de España. En caso de divergencia entre los participantes en la Promoción y la interpretación de las presentes Bases Legales por FERRERO, serán competentes para conocer de los litigios que puedan plantearse los Juzgados y Tribunales de la Ciudad de Barcelona (España), renunciando expresamente los participantes de esta Promoción a su propio fuero en caso de que lo tuvieren.</p>
-
+                    <h1 class="rem:text-[60px] font-hermes text-brown uppercase leading-[62px] tracking-[-0.6px] text-center mb-6">Bases legales</h1>
                 </div>
 
             </div>
@@ -102,6 +15,7 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
+import InsideHeaderPage from "@/Components/inside/insideHeaderPage.vue";
 
 const headerHeight = ref(0);
 
