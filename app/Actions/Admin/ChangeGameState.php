568fb09434ffd45752bf68dc3c37c384cd918418
Gerard A <gerard.a.c@gmail.com>
2024-09-26 15:47:54 +0200

    lost email

--- app/Actions/Admin/ChangeGameState.php
+++ app/Actions/Admin/ChangeGameState.php
@@ -4,6 +4,7 @@ namespace App\Actions\Admin;
 
 use App\Jobs\SendMail;
 use App\Mail\Confirmed;
+use App\Mail\Lost;
 use App\Mail\MoreInfo;
 use App\Mail\Winner;
 use App\Models\Game;
@@ -38,6 +39,7 @@ class ChangeGameState
         if ( $futureState === GameState::Denied ) {
             $game->decline_reason = (string)request('message');
             $game->update(['validated_at' => Carbon::now()]);
+            $this->sendLoserMail($game);
         }
 
         $game->update([
@@ -54,8 +56,9 @@ class ChangeGameState
         if ( $futureState === GameState::Winner ){
             $game->update(['confirmed_at' => Carbon::now()]);
             $this->sendConfirmedMail($game);
-        } else
-            SendMail::dispatchAfterResponse($game, $type);
+        }
+        //else
+            //SendMail::dispatchAfterResponse($game, $type);
 
         return response(null, 200);
     }
@@ -67,7 +70,12 @@ class ChangeGameState
 
     private function sendWinnerMail(Game $game)
     {
-            Mail::to([['name' => $game->user->name, 'email' => $game->user->email]])->send(new MoreInfo($game->token, '0'));
+        Mail::to([['name' => $game->user->name, 'email' => $game->user->email]])->send(new MoreInfo($game->token, '0'));
+    }
+
+    private function sendLoserMail(Game $game): void
+    {
+        Mail::to([['email' => $game->user->email, 'name' => $game->user->name]])->send(new Lost($game->decline_reason));
     }
 
 }
