51d86b22a0bd76fa6b3e839e0b5cb52a0e0451bc
Gerard A <gerard.a.c@gmail.com>
2025-03-13 10:20:16 +0100

    chatbot

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -26,6 +26,10 @@
         };
     </script>
 
+    <script>
+        (function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="Xq47mWRB1Usl94Hqt0mVh";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
+    </script>
+
     @routes
     @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
     @inertiaHead
