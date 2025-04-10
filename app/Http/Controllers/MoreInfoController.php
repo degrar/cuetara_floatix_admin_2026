3855ad53f77f4fbb08858700540ebe102f36f9c3
Gerard A <gerard.a.c@gmail.com>
2025-04-10 11:14:09 +0200

    acceptar dni en plataformas

--- app/Http/Controllers/MoreInfoController.php
+++ app/Http/Controllers/MoreInfoController.php
@@ -64,7 +64,7 @@ class MoreInfoController extends Controller
             ]);
         }
 
-        if ( $prize === 1 && ($type === 0 || $type === 3 || $type === 4)) {
+        if (($type === 0 || $type === 3 || $type === 4)) {
             $hashDniFront = request()->file('front')->store('files');
             File::query()->create([
                 'user_id' => $game->user_id,
